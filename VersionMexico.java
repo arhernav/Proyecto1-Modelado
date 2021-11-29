de4799a3a3e77591fc13d7b87c41459bba7a7bde
Armando Hernández Navarro <arhernav@ciencias.unam.mx>
2021-11-29 00:52:34 -0600

    Se arreglaron algunos errores discordias entre clases y se agregaron metodos auxiliares a algunas de las clases

--- VersionMexico.java
+++ VersionMexico.java
@@ -1,13 +1,13 @@
+import java.time.LocalDate;
+
 /**
  * Clase que modela la versión mexicana de la tienda CheemsMart.
  */
-public class VersionMexico extends VersionTienda{
+public class VersionMexico implements VersionTienda{
   
   /**
    * Método que le da la bienvenida al cliente mexicano al ingresar a la tienda.
-   */
-  import java.time.LocalDate;
-  
+   */  
   @Override
   public void saludar(){
     System.out.println("BIENVENID@ A CHEEMSMART!!!");
@@ -17,7 +17,7 @@ public class VersionMexico extends VersionTienda{
    * Método que da la despedida al cliente mexicano al salir de la tienda.
    */
   @Override
-  public void despedida(){
+  public void despedirse(){
     System.out.println("*Gracias por tu compra* \n\n¡VUELVE PRONTO!");
   }
   
@@ -25,9 +25,9 @@ public class VersionMexico extends VersionTienda{
    *Método que genera la fecha estimada de entrega.
    */
   @Override
-  public void generarFecha(){
+  public void generarFechaEntrega(){
     LocalDate fecha = LocalDate.now();
-    System.out.println(fecha.plusDays(15);
+    System.out.println(fecha.plusDays(15));
   }
   
   /**
